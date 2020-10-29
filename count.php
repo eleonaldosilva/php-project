c177346f975adb3ac7420133209a1a8343cc0523
Gabriel Dznvolve <gabriel.desenvolve@gmail.com>
2020-10-29 18:11:11 -0300

    creating a loop that will go through the selected file using a feof pointer

--- count.php
+++ count.php
@@ -1,5 +1,17 @@
 <?php
+// create a variable that fetches the file with fopen
 $file = fopen("./file.txt", "r");
+
+//I start a variable to take account of the lines and characters
 $lines = 0;
+
+//make a loop to go through the file line by line to the end of the file
+while (!feof($file)) {
+  //if extract a line from the file and it is not false
+  if ($line = fgets($file)) {
+    //accumulate one in the variable number of lines
+    $lines++;
+  }
+}
 fclose($file);
-echo "Linhas:  " . $lines . "<br>";
+echo "Lines:  " . $lines . "<br>";
