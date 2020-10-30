e370b036e2673c89926c1e8eb58e9429a2d9c4af
MatheusSaS <matheus.souza410@etec.sp.gov.br>
2020-10-29 22:13:36 -0300

    Solved exercise #5

---
+++ ip.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.scss">
+    <title>Document</title>
+
+<body>
+
+</body>
+
+</html>
+
+<?PHP
+
+function getUserIP()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if (filter_var($client, FILTER_VALIDATE_IP)) {
+        $ip = $client;
+    } elseif (filter_var($forward, FILTER_VALIDATE_IP)) {
+        $ip = $forward;
+    } else {
+        $ip = $remote;
+    }
+
+    return $ip;
+}
+
+$user_ip = getUserIP();
+echo "<div class='box'>";
+echo " <div class='wave -one'></div>";
+echo " <div class='wave -two'></div>";
+echo " <div class='wave -three'></div>";
+echo " <div class='title'>" . $user_ip . "</div>";
+echo "</div>";
