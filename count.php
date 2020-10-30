b75fc007b6d941065cb075440109c750f52acbd3
pedrozonta21 <pedrozonta6@gmail.com>
2020-10-29 21:22:06 -0300

    Nothing changed in the code, only the comments in English, to do a tiny change improving the gramatic, and to test pull requests

--- count.php
+++ count.php
@@ -2,10 +2,10 @@
 // create a variable that fetches the file with fopen
 $file = fopen("./file.txt", "r");
 
-//I start a variable to take account of the lines and characters
+//I started a variable to take count of the lines and characters
 $lines = 0;
 
-//make a loop to go through the file line by line to the end of the file
+//make a loop to go through the file's lines to the end of it
 while (!feof($file)) {
   //if extract a line from the file and it is not false
   if ($line = fgets($file)) {
