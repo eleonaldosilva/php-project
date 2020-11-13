81907b36826f11aa2bfe1bce56cdec102d3be586
Gabriel Rodrigues <gabriel.desenvolve@gmail.com>
2020-11-13 16:56:36 -0300

    Update: arrays increasing and decreasing

--- array5.php
+++ array5.php
@@ -1,9 +1,16 @@
 <!--I have a property in PHP called: rsort -->
 <!-- rsort example -->
-<?php
-$fruits = array("lemon", "orange", "banana", "apple");
+<!-- $fruits = array("lemon", "orange", "banana", "apple");
+// sort($fruits);
 rsort($fruits);
 foreach ($fruits as $key => $value) {
   echo "$key = $value\n";
-}
+} -->
+<?php
+$array = array('A', 'B', 'C', 'D', 'E');
+sort($array); // Show array in ascending order
+print_r($array);
+echo '<br/>';
+rsort($array); // show array in descending order
+print_r($array);
 ?>
