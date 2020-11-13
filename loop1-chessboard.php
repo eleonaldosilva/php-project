3e43787bf311a344783aba59f67e49147ef800e0
Donato Junior <donato.junior.07@gmail.com>
2020-11-12 21:58:20 -0300

    Boilerplate done

---
+++ loop1-chessboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Chess Board</title>
+</head>
+<body>
+  <h1>Chess Board PHP</h1>
+  <?php
+    echo "<h1>Hello world</h1>"
+  ?>
+</body>
+</html>
