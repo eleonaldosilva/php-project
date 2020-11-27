a2fdbaaf5d5324c861d50bd3e74b02ebb6b90a72
gabrielMonteiroDias <73367847+gabrielMonteiroDias@users.noreply.github.com>
2020-11-26 21:26:15 -0300

    Loop3 solved

---
+++ loop3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.scss">
+    <title>Document</title>
+
+<body>
+
+</body>
+
+</html>
+
+
+<?PHP
+
+/*
+$str = "Você tem que abrir seu coração pro SQL e pedir o que realmente que qualque."; 
+preg_match_all('/\bque\b/i', $str, $ocorrencias);
+echo count($ocorrencias[0]);
+*/
+//---------------
+
+$fatec = "fatec tecnologia";
+echo substr_count($fatec, 't');
+?>
